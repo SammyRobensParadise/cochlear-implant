76e61d1b457b94c28893b102850e8bffa58e4e03
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-10-19 14:10:04 -0400

    YEEEEEEEEEEEEEEEEET 👍 😇

--- write_audio_to_new_file.m
+++ write_audio_to_new_file.m
@@ -1,5 +1,5 @@
-function write_audio_to_new_file(testAudio, testAudioSampleRate)
-newAudioFile = './newAudio.wav';
+function write_audio_to_new_file(testAudio, testAudioSampleRate,filename)
+newAudioFile = filename;
 audiowrite(newAudioFile,testAudio,testAudioSampleRate);
 clear testAudio testAudioSampleRate;
 end

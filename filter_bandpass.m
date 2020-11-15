d4a659b125c167e6d6a7b30cf25226722c629f03
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-11-14 20:36:25 -0500

    createdf bandpass filter

--- filter_bandpass.m
+++ filter_bandpass.m
@@ -1,5 +1,6 @@
 function [interval_audio_matrix] = filter_bandpass(audio_matrix,min_freq_range,max_freq_range)
 %FILTER_BANDPASS returns an audio matrix of the specified sub_interval
+
 interval_audio_matrix = audio_matrix;
 end
 

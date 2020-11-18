987a764973fd54c0271d335eb3a16780eb0f771d
Mary McPhee <maryrsmcphee@gmail.com>
2020-11-18 14:25:55 -0500

    changes to filter and envelope array

--- detect_envelope.m
+++ detect_envelope.m
@@ -1,8 +1,18 @@
-function [envelope_array] = detect_envelope(inputArg1,inputArg2)
+function [envelope_array] = detect_envelope(rectified_array)
 %DETECT_ENVELOPE detects envelope of each signal in rectified array then
 %sorts into new array (envelope_array). Uses lowpass_filter.m
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
 
+% filter specifications
+order  = 10;   % Order
+cutoff_frequency = 400;  % Cutoff Frequency
+
+% create array for filtered sounds
+array_length=length(rectified_array);
+envelope_cells=cell(array_length,1);
+
+% index through the rectified array and add to envelope array
+for c = 1:array_length
+    envelope_cells{c}=lowpass_filter(rectified_array{c}, order, cutoff_frequency);
+envelope_array = envelope_cells;
+end
--- lowpass_filter.m
+++ lowpass_filter.m
@@ -1,12 +1,7 @@
-function lp_filter = lowpass_filter
+function [lp_filter] = lowpass_filter(sampling_frequency, order, cutoff_frequency )
 %LOWPASS_FILTER Returns a discrete-time filter object.
 
 % Butterworth Lowpass filter designed using FDESIGN.LOWPASS.
-
-% All frequency values are in Hz.
-sampling_frequency = 1600;  % Sampling Frequency
-order  = 10;   % Order
-cutoff_frequency = 400;  % Cutoff Frequency
 
 % Construct an FDESIGN object and call its BUTTER method.
 lowpass_specs  = fdesign.lowpass('N,F3dB', order, cutoff_frequency, sampling_frequency);

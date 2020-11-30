a4e47cf55f73a6ef3538c764135c023a465d2913
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-11-30 18:00:00 -0500

    added modulation function

--- modulate_signals.m
+++ modulate_signals.m
@@ -1,8 +1,14 @@
 function [isModulated,modulated_signal_cells] = modulate_signals(information_signal,carrier_signal)
 % MODULATE_SIGNAL modulates an information signal with a cosine
 % carrier signal
+
 indexer=size(information_signal);
+indexer=indexer(1);
+signal_cells=cell(indexer,1);
+for c=1:indexer
+    signal_cells{c}=carrier_signal{c}*information_signal; 
+modulated_signal_cells = signal_cells;
 isModulated = information_signal;
-modulated_signal_cells = carrier_signal;
+plot(modulated_signal_cells{1})
 end
 
